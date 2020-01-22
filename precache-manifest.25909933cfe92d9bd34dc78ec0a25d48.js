90d6e6542ed03db95671f8d9649a78ca9e02758a
wallac6 <wallac6@stolaf.edu>
2020-01-22 10:27:19 +0000

    Updates

---
+++ precache-manifest.25909933cfe92d9bd34dc78ec0a25d48.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6215871cfa11e1968d1",
+    "url": "/rewards-project/static/css/main.1d8d7695.chunk.css"
+  },
+  {
+    "revision": "a6215871cfa11e1968d1",
+    "url": "/rewards-project/static/js/main.a6215871.chunk.js"
+  },
+  {
+    "revision": "0a01b32b919826e0deb9",
+    "url": "/rewards-project/static/js/runtime~main.0a01b32b.js"
+  },
+  {
+    "revision": "ce1cdd22971436afbcf4",
+    "url": "/rewards-project/static/js/2.ce1cdd22.chunk.js"
+  },
+  {
+    "revision": "f517d96b559f17890451b14638aae7fc",
+    "url": "/rewards-project/static/media/bananas.f517d96b.png"
+  },
+  {
+    "revision": "e5f5de89d6aebe71185f3b5dc745f83b",
+    "url": "/rewards-project/static/media/apple.e5f5de89.png"
+  },
+  {
+    "revision": "c055f75785a8208fe3ca213ae5679a50",
+    "url": "/rewards-project/static/media/pineapple.c055f757.png"
+  },
+  {
+    "revision": "7724a471231f40535e2653d0210b0e4e",
+    "url": "/rewards-project/static/media/jackfruit.7724a471.png"
+  },
+  {
+    "revision": "dee17e1cdc3cc70abf2d71952964a1aa",
+    "url": "/rewards-project/static/media/mango.dee17e1c.png"
+  },
+  {
+    "revision": "79525a35244b5aa039cc91dc88dc9382",
+    "url": "/rewards-project/static/media/OAK.79525a35.png"
+  },
+  {
+    "revision": "a4d2885cbb8d2af18748b06d13fa2537",
+    "url": "/rewards-project/static/media/PALM.a4d2885c.png"
+  },
+  {
+    "revision": "740d627868228dc198bfc1d5e4cf0518",
+    "url": "/rewards-project/static/media/BONSAI.740d6278.png"
+  },
+  {
+    "revision": "d4c5279a63e14faebacffc95e6db32b4",
+    "url": "/rewards-project/static/media/BAOBAB.d4c5279a.png"
+  },
+  {
+    "revision": "9d290d5aca48e5a07a5fc63e7a584ae4",
+    "url": "/rewards-project/static/media/DRAGONBLOOD.9d290d5a.png"
+  },
+  {
+    "revision": "24f7c12f239e67e4337dfe1bac0dec83",
+    "url": "/rewards-project/index.html"
+  }
+];
